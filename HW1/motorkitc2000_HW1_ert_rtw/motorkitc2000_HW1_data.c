cba425f8c07763e54f050ec3cbf4ec8792877464
pruthakjoshi <pruthakjoshi@gmail.com>
2023-02-01 15:53:23 -0800

    sdfiosa

---
+++ HW1/motorkitc2000_HW1_ert_rtw/motorkitc2000_HW1_data.c
@@ -0,0 +1,79 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: motorkitc2000_HW1_data.c
+ *
+ * Code generated for Simulink model 'motorkitc2000_HW1'.
+ *
+ * Model version                  : 2.5
+ * Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+ * C/C++ source code generated on : Wed Feb  1 15:49:41 2023
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Texas Instruments->C2000
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "motorkitc2000_HW1.h"
+
+/* Block parameters (default storage) */
+P_motorkitc2000_HW1_T motorkitc2000_HW1_P = {
+  /* Expression: 958.0466360856269
+   * Referenced by: '<Root>/Constant'
+   */
+  958.04663608562691,
+
+  /* Expression: 1
+   * Referenced by: '<Root>/Constant1'
+   */
+  1.0,
+
+  /* Expression: -100000
+   * Referenced by: '<Root>/Constant4'
+   */
+  -100000.0,
+
+  /* Expression: 3.14159/400
+   * Referenced by: '<Root>/Gain'
+   */
+  0.007853975,
+
+  /* Expression: 1000
+   * Referenced by: '<Root>/Constant2'
+   */
+  1000.0,
+
+  /* Expression: 1
+   * Referenced by: '<Root>/Step'
+   */
+  1.0,
+
+  /* Expression: 0
+   * Referenced by: '<Root>/Step'
+   */
+  0.0,
+
+  /* Expression: 1
+   * Referenced by: '<Root>/Step'
+   */
+  1.0,
+
+  /* Expression: 10.48
+   * Referenced by: '<Root>/Output voltage to PWM1'
+   */
+  10.48,
+
+  /* Expression: 1000/10.48
+   * Referenced by: '<Root>/Output voltage to PWM'
+   */
+  95.419847328244273
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ HW1/motorkitc2000_HW1_ert_rtw/motorkitc2000_HW1_private.h
@@ -0,0 +1,54 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: motorkitc2000_HW1_private.h
+ *
+ * Code generated for Simulink model 'motorkitc2000_HW1'.
+ *
+ * Model version                  : 2.5
+ * Simulink Coder version         : 9.8 (R2022b) 13-May-2022
+ * C/C++ source code generated on : Wed Feb  1 15:49:41 2023
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Texas Instruments->C2000
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_motorkitc2000_HW1_private_h_
+#define RTW_HEADER_motorkitc2000_HW1_private_h_
+#include "rtwtypes.h"
+#include "multiword_types.h"
+#include "motorkitc2000_HW1_types.h"
+
+/* Private macros used by the generated code to access rtModel */
+#ifndef rtmIsMajorTimeStep
+#define rtmIsMajorTimeStep(rtm)        (((rtm)->Timing.simTimeStep) == MAJOR_TIME_STEP)
+#endif
+
+#ifndef rtmIsMinorTimeStep
+#define rtmIsMinorTimeStep(rtm)        (((rtm)->Timing.simTimeStep) == MINOR_TIME_STEP)
+#endif
+
+#ifndef rtmSetTFinal
+#define rtmSetTFinal(rtm, val)         ((rtm)->Timing.tFinal = (val))
+#endif
+
+#ifndef rtmSetTPtr
+#define rtmSetTPtr(rtm, val)           ((rtm)->Timing.t = (val))
+#endif
+
+void config_QEP_eQEP1(uint32_T pcmaximumvalue, uint32_T pcInitialvalue, uint32_T
+                      unittimerperiod, uint32_T comparevalue, uint16_T
+                      watchdogtimer, uint16_T qdecctl, uint16_T qepctl, uint16_T
+                      qposctl, uint16_T qcapctl, uint16_T qeint);
+
+#endif                             /* RTW_HEADER_motorkitc2000_HW1_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
