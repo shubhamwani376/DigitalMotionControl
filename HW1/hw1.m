b3f020d3c74aa1e6599a1b3eb02b91f0ff3a17ea
Shubham Wani <45815852+shubhamwani376@users.noreply.github.com>
2023-02-01 13:58:28 -0800

    Update hw1.m

--- HW1/hw1.m
+++ HW1/hw1.m
@@ -1,31 +1,33 @@
-close all;
+close all; clc; clear;
 load("encposition_step_normalized_second.mat");
 position = data.extractTimetable.Sum2;
-position = position(5193:16000);
+position = position(18000:end);
 timep = 0.001;
-time = (0:timep:16.000-5.193)';
+time = (0:timep:(size(position)/1000))';
 %plot(position);
+
 % hold on
 vel = smooth(diff(position)/timep)*(2*pi()/400);
-scatter(time(2:end),vel, 'filled');
+plot(vel)
+%scatter(time(2:end),vel, 'filled');
 
-f = fittype('k*(1 - exp(-x/tau))');
-[fit1,gof,fitinfo] = fit(time(2:end),vel,f,'StartPoint',[427 0.2]);
+f = fittype('k*(exp(-x/tau))');
+[fit1,gof,fitinfo] = fit(time(3:end),vel,f,'StartPoint',[5 70]);
 %params = fittype(k/(t*s + 1))
 stepinfo(vel)
 
-load("encposition_step_normalized_third.mat");
-position = data.extractTimetable.Sum2;
-position = position(6216:12000);
-timep = 0.001;
-time = (0:timep:12.000-6.216)';
-% plot(position);
-% hold on
-vel = smooth(diff(position)/timep)*(2*pi()/400);
-scatter(time(2:end),vel, 'filled');
-
-f = fittype('k*(1 - exp(-x/tau))');
-[fit1,gof,fitinfo] = fit(time(2:end),vel,f,'StartPoint',[427 0.2]);
-%params = fittype(k/(t*s + 1))
-% [transferfunc, f] = tfestimate(ones(10807,1), vel);
-% %plot(f,mag2db(abs(transferfunc)));
+% load("encposition_step_normalized_third.mat");
+% position = data.extractTimetable.Sum2;
+% position = position(6216:12000);
+% timep = 0.001;
+% time = (0:timep:12.000-6.216)';
+% % plot(position);
+% % hold on
+% vel = smooth(diff(position)/timep)*(2*pi()/400);
+% scatter(time(2:end),vel, 'filled');
+% 
+% f = fittype('k*(1 - exp(-x/tau))');
+% [fit1,gof,fitinfo] = fit(time(2:end),vel,f,'StartPoint',[427 0.2]);
+% %params = fittype(k/(t*s + 1))
+% % [transferfunc, f] = tfestimate(ones(10807,1), vel);
+% % %plot(f,mag2db(abs(transferfunc)));
