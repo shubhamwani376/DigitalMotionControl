b75e115bf7b0d772dcdb10f611a176eb3a80bbaf
pruthakjoshi <pruthakjoshi@gmail.com>
2023-02-20 20:53:30 -0800

    plots

--- HW2/New Data/graphs.m
+++ HW2/New Data/graphs.m
@@ -1,10 +1,10 @@
 clear all
 close all
-load('pendulum_osc_tri_100.mat')
+load('pendulum_integral_tri_100.mat')
 position  = plant_output.extractTimetable.Gain;
 position = nonzeros(position);
 
-input = tri_input.extractTimetable;
+input = triangle_input.extractTimetable;
 input = input.Variables;
 input = nonzeros(input);
 
@@ -12,11 +12,11 @@ input = nonzeros(input);
 plot(0:0.01:5,position(1:501), LineWidth=2)
 hold on
 grid on
-plot(0:0.01:5,input(1:01), LineWidth=1)
+plot(0:0.01:5,input(1:501), LineWidth=1)
 xlabel('Time (sec)')
 ylabel('Position (rad)')
 legend('Motor Position', 'Reference')
-title('Integral + Oscillator 100Hz')
+title('Integral 100Hz')
 
 
 
