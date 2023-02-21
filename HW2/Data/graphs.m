7d5bc1c3e02f2f3418631fef5c20039b29af96fe
pruthakjoshi <pruthakjoshi@gmail.com>
2023-02-20 17:49:17 -0800

    plots

--- HW2/Data/graphs.m
+++ HW2/Data/graphs.m
@@ -1,6 +1,6 @@
 clear all
 close all
-load('pendulum_integral_sine_1000.mat')
+load('pendulum_integral_sine_100.mat')
 position  = plant_output.extractTimetable.Gain;
 position = nonzeros(position);
 
@@ -9,14 +9,14 @@ input = input.Variables;
 input = nonzeros(input);
 
 %%
-plot(0:0.001:10,position(1:10001), LineWidth=2)
+plot(0:0.01:5,position(1:501), LineWidth=2)
 hold on
 grid on
-plot(0:0.001:10,input(1:10001), LineWidth=1)
+plot(0:0.01:5,input(1:501), LineWidth=1)
 xlabel('Time (sec)')
 ylabel('Position (rad)')
 legend('Motor Position', 'Reference')
-title('Integral 1000Hz')
+title('Integral 100Hz')
 
 
 
