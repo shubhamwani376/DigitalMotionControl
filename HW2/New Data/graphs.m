b88a0f2061a5906d16a79541e330029e0242bfd0
pruthakjoshi <pruthakjoshi@gmail.com>
2023-02-22 15:35:50 -0800

    plots

--- HW2/New Data/graphs.m
+++ HW2/New Data/graphs.m
@@ -3,18 +3,18 @@ close all
 %load('lqg_exp_sine_100.mat')
 
 %%
-position  = sine_output.extractTimetable.Gain;
+position  = triangle_output.extractTimetable.Gain;
 %position = nonzeros(position);
 
-input = sine_input.extractTimetable;
+input = triangle_input.extractTimetable;
 input = input.Variables;
 %input = nonzeros(input);
 
 %%
-plot(0:0.01:5,position(2200:2700), LineWidth=2)
+plot(0:0.01:5,position(974:1474), LineWidth=2)
 hold on
 grid on
-plot(0:0.01:5,input(2200:2700), LineWidth=1)
+plot(0:0.01:5,input(974:1474), LineWidth=1)
 xlabel('Time (sec)')
 ylabel('Position (rad)')
 legend('Motor Position', 'Reference')
